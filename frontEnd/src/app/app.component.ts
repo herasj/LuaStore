daea717d80dfb588d31cf598df80ffbe7f749c66
Leonrdarc <leonardoarchila@hotmail.com>
2019-09-29 23:46:19 -0500

    Logo/UserTile and CartTile
    Añadidas funcionalidades responsive a la navbar y el tile del usuario para inicar sesion o ver informacion del perfil y  el tile del carrito de compras.

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+
   title = 'frontEnd';
+  isInMobile: boolean;
+  showingUser: boolean;
+  showingCart: boolean;
+  ngOnInit(){
+    console.log("init en sakgas")
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+    if(event.toElement.id=="shopping") {
+      this.showingCart = true;
+      this.showingUser = false;
+      console.log(this.showingUser);
+    }
+    else if(event.toElement.id=="user") {
+      this.showingUser = true;
+      this.showingCart = false;
+      console.log(this.showingUser);
+    }
+    else {
+      this.showingUser = false
+      this.showingCart = false;
+      console.log(event);
+    };
+  }
+
+  onResize(event){
+    if(event.target.innerWidth<768) this.isInMobile=true;
+    else this.isInMobile=false;
+    console.log("resize");
+    this.showingUser = false
+    this.showingCart = false;
+  }
+
+  firstResize(){
+
+  }
+
+  setVisible(visible: boolean){
+    this.showingUser=visible;
+  }
+  
 }
