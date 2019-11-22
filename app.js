755ca65f234ef462a0219030aa1c21eeaeae1704
Juan Rambal <juanp231100@gmail.com>
2019-11-22 00:40:50 -0500

    CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 //RUTAS
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors({ origin: '*', optionsSuccessStatus: 200 }))
 //RUTAS
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
