b4c82bcb6d909305fccf5e4e9d405b8877cf7a9f
Juan Rambal <juanp231100@gmail.com>
2019-11-22 12:02:35 -0500

    get product with photo

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -35,7 +35,10 @@ module.exports = {
         sql.connect(config, function (err) {
             if (err){console.log(err); res.send(errmsg);}//Mostrar error si existe    
             var request = new sql.Request();
-            var query=`SELECT * FROM Productos p WHERE p.Nombre='${data.name}'`;
+            var query=`SELECT *
+            FROM Productos p JOIN Fotos f 
+            ON p.productID=f.productID 
+            WHERE p.Nombre='${data.name}'`;
             // Aplicar query a la db y guardar en recordset
             request.query(query, function (err, recordset) {
                 if (err) console.log(err) //Si hay error mostrar
@@ -43,7 +46,7 @@ module.exports = {
                 sql.close();
             });
         });
-    },
+    }, //https://imgur.com/JMmL824
     new: (res)=>{
         sql.connect(config, function (err) {
             if (err){console.log(err); res.send(errmsg);}//Mostrar error si existe    
